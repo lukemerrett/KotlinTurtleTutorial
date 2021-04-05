6f6145f8303a56cdb2c06c4b50d945d1aa83d3c6
Luke Merrett <lukeamerrett@gmail.com>
2021-04-05 12:00:53 +0100

    Finished process command function

--- src/TurtleTutorial.kt
+++ src/TurtleTutorial.kt
@@ -1,19 +1,42 @@
 package turtle_tutorial
 
+import kotlin.math.*
+
 enum class PenState { Up, Down }
 enum class Colour { Black, Red, Blue, Green }
-enum class TurnDirection { Left, Right }
+enum class Direction { Left, Right }
 
 data class Turtle (
-    val xpos: Float,
-    val ypos: Float,
-    val angle: Float,
+    val xpos: Double,
+    val ypos: Double,
+    val angle: Double,
     val penState: PenState,
     val colour: Colour)
 
 interface Command
 
-data class Move (val distance: Float) : Command
-data class Turn (val direction: TurnDirection, val degrees: Float) : Command
+data class Move (val distance: Double) : Command
+data class Turn (val direction: Direction, val degrees: Double) : Command
 data class SetPen (val penState: PenState) : Command
 data class SetColour (val colour: Colour) : Command
+
+fun processCommand (turtle: Turtle, command: Command): Turtle {
+    return when (command) {
+        is Move -> {
+            val angleInRads = turtle.angle * (PI /180.0)
+            return turtle.copy(
+                xpos = turtle.xpos + command.distance * sin(angleInRads),
+                ypos = turtle.ypos + command.distance * cos(angleInRads)
+            )
+        }
+        is Turn -> {
+            return when (command.direction) {
+                Direction.Left -> turtle.copy(angle = turtle.angle - command.degrees)
+                Direction.Right -> turtle.copy(angle = turtle.angle + command.degrees)
+            }
+        }
+        is SetPen -> turtle.copy(penState = command.penState)
+        is SetColour -> turtle.copy(colour = command.colour)
+        else -> turtle
+    }
+}
